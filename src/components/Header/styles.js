24b1f2a45eb64216bea7bef69610ae8a588c613c
lucsmac <lucas.macedo0821@gmail.com>
2020-08-02 12:24:24 -0300

    toggle theme implemented

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -4,7 +4,9 @@ export const Container = styled.div`
   height: 60px;
   background: ${props => props.theme.colors.primary};
   color: #FFF;
-  display: flex;
   align-items: center;
   padding: 0 30px;
+  
+  display: flex;
+  justify-content: space-between;
 `
---
+++ src/styles/themes/dark.js
@@ -0,0 +1,11 @@
+export default {
+  title: 'dark',
+
+  colors: {
+    primary: '#333',
+    secundary: '#C62E65',
+
+    background: '#222',
+    color: '#F5F5F5'
+  }
+}
